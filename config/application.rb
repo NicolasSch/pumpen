f774f94a0500eb39cccb4c1d1a880f3c923414bc
Nicolas Schwartau <n.schwartau@gmail.com>
2017-03-26 23:13:57 +0200

    locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Pumpen
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = [:de, :en]
     I18n.default_locale = :de
   end
 end
