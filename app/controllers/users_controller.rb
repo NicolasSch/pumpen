f7083c39eaac85544f60084ca4ae81fbdea17bba
Nicolas Schwartau <n.schwartau@gmail.com>
2017-03-26 22:40:00 +0200

    updates password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,41 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = current_user
-    authorize! :write, @user
+  @user = User.find(params[:id])
+  authorize! :write, @user
 
-    if (params[:password].present? ? @user.update_with_password(user_params) : @user.update(user_params))
-      redirect_to edit_user_path(@user), notice: t('user.profile.notice.save')
+  if update_user
+    bypass_sign_in(@user)
+    redirect_to edit_user_path(@user), notice: @notice
+  else
+    flash[:alert] = t('user.profile.alert')
+    render :edit
+  end
+end
+
+private
+
+  def update_user
+    if save_password?
+      @notice = t('user.profile.password_save')
+      @user.update_with_password(user_params) if validate_password_params
     else
-      render :edit
+      @notice = t('user.profile.data_save')
+      @user.update(user_params.except(:password, :current_password, :password_confirmation))
     end
+  end
+
+  def save_password?
+    user_params.key?(:password) ||
+      user_params.key?(:password_confirmation) ||
+      user_params.key?(:current_password)
+  end
+
+  def validate_password_params
+    @user.errors.add(:current_password, :blank) unless user_params[:current_password].present?
+    @user.errors.add(:password, :blank) unless user_params[:password].present?
+    @user.errors.add(:password_confirmation, :blank) unless user_params[:password_confirmation].present?
+    !@user.errors.any?
   end
 
   private
