db3660d470a2cc79ae2e4418edb9525ac3cb7532
Nicolas Schwartau <niggo@Beefcakes.local>
2017-05-26 13:18:32 +0200

    sets mail configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV['SMTP_DOMAIN'] }
+
+  config.action_mailer.smtp_settings = {
+    user_name:        ENV['SMTP_USER'],
+    password:         ENV['SMTP_PASSWORD'],
+    address:          ENV['SMTP_ADDRESS'],
+    domain:           ENV['SMTP_DOMAIN'],
+    port:             ENV['SMTP_PORT'],
+    authentication:   ENV['SMTP_AUTH'],
+    enable_starttls_auto: true
+  }
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+  config.x.redis_url = ENV['REDISCLOUD_URL']
+
+  # redirect to https
+  config.force_ssl = true
 end
