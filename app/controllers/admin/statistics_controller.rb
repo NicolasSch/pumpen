f392a1ee36b4dde8c8243739e113861e3b7bb4f4
Nicolas Schwartau <n.schwartau@gmail.com>
2017-07-29 22:01:01 +0200

    adds new view to see stistics - for now just sold products with filter functions

---
+++ app/controllers/admin/statistics_controller.rb
@@ -0,0 +1,22 @@
+class Admin::StatisticsController < AdminController
+  include SmartListing::Helper::ControllerExtensions
+  helper  SmartListing::Helper
+
+  before_action :set_start, :set_end, only: :index
+
+  def index
+    products_scope = Product.sold_in_with_quantity(@start.to_datetime, @end.to_datetime)
+    products_scope = products_scope.where(product_group: params[:filter]) if params[:filter].present?
+    @products = smart_listing_create(:products, products_scope, partial: "admin/statistics/sold_product", default_sort: { title: 'asc' })
+  end
+
+  def set_start
+    @start ||= Date.today.at_beginning_of_month - 1.month
+    @start = Date.parse(params[:start]) if params[:start].present?
+  end
+
+  def set_end
+    @end ||= Date.today.at_end_of_month - 1.month
+    @end = Date.parse(params[:end]) if params[:end].present?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     end
     resources :users
     resources :products
+    resources :statistics, only: :index
     resources :tabs, only: [:index, :update]
     resources :bills, only: [:index, :show, :create, :update]
     resources :shops, only: :index
