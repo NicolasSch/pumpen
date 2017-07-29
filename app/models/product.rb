b622139c650d74c9fee4e64b4fbc02db01bae732
Nicolas Schwartau <n.schwartau@gmail.com>
2017-07-29 21:59:51 +0200

    adds scope to find products bought in a given timespan

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ActiveRecord::Base
   has_many :tab_items, dependent: :destroy
+  has_many :tabs, through: :tab_items
   validates :price, :title, presence: true
   validates :title, uniqueness: true
 
   scope :active,    -> { where(archived: false) }
   scope :archived,  -> { where(archived: true) }
+
+  def self.sold_in_with_quantity(start_time, end_time)
+    joins(:tab_items)
+      .where('tab_items.created_at >  ? AND tab_items.created_at < ? AND tab_id IS NOT NULL', start_time, end_time)
+      .select("products.id, plu, title, product_group, product_group_id, product_type, SUM(quantity) AS sum_quantity, (price * SUM(quantity)) AS sum_price")
+      .group("tab_items.product_id")
+    end
 end
