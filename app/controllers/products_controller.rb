909a5c4b70e2f69d4da71890059c3793bb0b1406
Nicolas Schwartau <n.schwartau@gmail.com>
2017-07-30 13:56:56 +0200

    adds smart listing for product view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,23 @@
 class ProductsController < ApplicationController
+  include SmartListing::Helper::ControllerExtensions
+  helper  SmartListing::Helper
+
   before_action :ensure_cart, :set_cart
 
   def index
+    products_scope = params[:archived] == '1' ?  Product.archived : Product.active
+    products_scope = products_scope.where(product_group: params[:filter]) if params[:filter].present?
+    @products = smart_listing_create(:products, products_scope, partial: "admin/products/product", default_sort: { title: 'asc' })
+  end
+
+  def index
     authorize! :read, Product
-    @open_bills = current_user.bills.open
-    @products = Product.order(:title).active
-    @tab_item = TabItem.new
+    @tab_item      = TabItem.new
+    @open_bills    = current_user.bills.open
+    products_scope = Product.active
+    products_scope = products_scope.where(product_group: params[:filter]) if params[:filter].present?
+    @products      = smart_listing_create(:products, products_scope, partial: "/products/product", default_sort: { title: 'asc' })
+
   end
 
   private
