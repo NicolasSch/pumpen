97c8fd0906435aa24cbc4220a8d2f9f658d3e201
Nicolas Schwartau <n.schwartau@gmail.com>
2017-09-30 17:59:20 +0200

    Removes debug output

--- app/controllers/api/reminders_controller.rb
+++ app/controllers/api/reminders_controller.rb
@@ -1,8 +1,6 @@
 class Api::RemindersController < ApiController
   def create
     bills = Bill.where(paid: false)
-    puts 'waaaaaaa'
-    puts bills.count
     bills.each do |bill|
       bill.user.queue_open_bills_reminder
       bill.update!(reminded_at: Time.now)
