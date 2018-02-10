2c414df2d046a9781339b705c195d91f781a4ce7
Nicolas Schwartau <n.schwartau@gmail.com>
2018-02-10 14:02:59 +0100

    Makes fast tab addition async

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -9,11 +9,14 @@ class TabsController < ApplicationController
   end
 
   def update
-    tab = current_user.tabs.find(params[:id])
-    authorize! :write, tab
-    item = tab.add_product(params[:product_id])
-    if tab.save
-      redirect_to :root, notice: t('add_tab.add_product', product: item.product.title)
+    @tab = current_user.tabs.find(params[:id])
+    authorize! :write, @tab
+    item = @tab.add_product(params[:product_id])
+    if @tab.save
+      respond_to do |format|
+         format.js { flash.now[:notice] = t('add_tab.add_product', product: item.product.title) }
+         format.html { redirect_to :root, notice: t('add_tab.add_product', product: item.product.title) }
+       end
     else
       redirect_to :root, alert: 'Something went wrong'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   AVAILABLE_MEMBERSHIPS = %w(staff full four three two one guest)
   ROLES = %w(admin user)
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
